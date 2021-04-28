546329237e1d39ff49d107b3041cb3791f77aa40
Abraham <abdiaz2018@gmail.com>
2021-04-28 11:30:18 -0700

    edit and delete access

--- app/controllers/transformers_controller.rb
+++ app/controllers/transformers_controller.rb
@@ -8,7 +8,7 @@ class TransformerController < ApplicationController
     end
     post "/transformers" do 
         @transformer = Transformer.new(:name => params[:name], :alternate_form => params[:alternate_form], :team => params[:team])
-        @transformer.user_id
+        @transformer.user_id = current_user.id 
         @transformer.save
         redirect "/transformers/#{@transformer.id}"
     end
@@ -18,7 +18,34 @@ class TransformerController < ApplicationController
     end
 
     get "/transformers/:id/edit" do 
-        @transformer = Transformer.find(params[:id])
-        erb :"/transformers/edit"
+        @transformer = Transformer.find_by_id(params[:id])
+        if @transformer.user_id == current_user.id 
+            erb :"/transformers/edit"
+        else
+            erb :"/transformers/denied"
+        end
+    end
+
+    patch "/transformers/:id" do 
+        @transformer = Transformer.find_by_id(params[:id])
+        @transformer.name = params[:name]
+        @transformer.alternate_form = params[:alternate_form]
+        @transformer.team = params[:team]
+        @transformer.save 
+        redirect "/transformers/#{@transformer.id}"
+    end
+
+    delete "/transformers/:id" do 
+        
+        @transformer = Transformer.find_by_id(params[:id])
+        if @transformer.user_id == current_user.id 
+            @transformer.delete 
+            redirect "/transformers"
+        else
+            erb :"/transformers/denied"
+        end
+    end
+
+    
 
 end
